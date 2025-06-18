56a9c98667db705a273886603d8cfb0ce4b8ebab
Anto-Lezcano <lezcanoantonella85@gmail.com>
2025-06-18 15:54:16 -0300

    se trabajo en el men

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -4,6 +4,7 @@ import { UserModule } from "./users/user.module";
 import { AuthModule } from "./auth/auth.module";
 import { CouponModule } from "./coupons/coupon.module";
 import { ComboModule } from "./combos/combo.module";
+import { MenuModule } from "./menu/menu.module";
 @Module({
   imports: [
     MongooseModule.forRoot("mongodb://localhost:27017/mcdonald"),
@@ -11,6 +12,7 @@ import { ComboModule } from "./combos/combo.module";
     AuthModule,
     CouponModule,
     ComboModule,
+    MenuModule,
   ],
 })
 export class AppModule {}
